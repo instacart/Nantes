83e534de5fd36175d589072947ccc085701e3a32
Joe Spadafora <spadafiva@gmail.com>
2019-03-28 21:13:39 -0400

    Adds swiftlint to test sources and fixes linting errors

--- Nantes/NantesTests/AttributedStringExtensionTests.swift
+++ Nantes/NantesTests/AttributedStringExtensionTests.swift
@@ -6,23 +6,23 @@
 //  Copyright © 2019 Instacart. All rights reserved.
 //
 
-import XCTest
 @testable import Nantes
+import XCTest
 
 final class AttributedStringExtensionTests: XCTestCase {
-    
+
     private let testUrl = URL(string: "https://www.swiftjoe.com")!
-    
+
     func testReturnsLinksIfFoundInAttributedString() {
         let linkString = NSAttributedString(string: "Contains a link", attributes: [.link: testUrl])
         let existingLinks = linkString.findExistingLinks()
         XCTAssertFalse(existingLinks.isEmpty)
     }
-    
+
     func testFindsLinksForAttributedStringsCreatedFromHTML() {
         let htmlString = "<a href=\"http://www.google.com\">I can be clicked\""
         guard let data = htmlString.data(using: .utf8) else {
-            XCTFail()
+            XCTFail("Could not get data detector")
             return
         }
         do {
@@ -33,16 +33,16 @@ final class AttributedStringExtensionTests: XCTestCase {
             let existingLinks = attributedString.findExistingLinks()
             XCTAssert(existingLinks.count == 1)
         } catch {
-            XCTFail()
+            XCTFail("Could not get data detector")
         }
     }
-    
+
     func testReturnsLinkIsEmptyIfNoAttributedLink() {
         let linkString = NSAttributedString(string: "Contains no links at all")
         let checkingResults = linkString.findExistingLinks()
         XCTAssertTrue(checkingResults.isEmpty)
     }
-    
+
     func testCheckingResultsIsEmptyIfAttributedStringContainsAttributedLink() {
         do {
             let detector = try NSDataDetector(types: NantesLabel().enabledTextCheckingTypes.rawValue)
@@ -50,10 +50,10 @@ final class AttributedStringExtensionTests: XCTestCase {
             let checkingResults = linkString.findCheckingResults(usingDetector: detector)
             XCTAssertTrue(checkingResults.isEmpty)
         } catch {
-            XCTFail()
+            XCTFail("Could not get data detector")
         }
     }
-    
+
     func testCheckingResultsContainsValueIfDataFoundInString() {
         do {
             let detector = try NSDataDetector(types: NantesLabel().enabledTextCheckingTypes.rawValue)
@@ -61,7 +61,7 @@ final class AttributedStringExtensionTests: XCTestCase {
             let checkingResults = linkString.findCheckingResults(usingDetector: detector)
             XCTAssert(checkingResults.count == 1)
         } catch {
-            XCTFail()
+            XCTFail("Could not get data detector")
         }
     }
 }
