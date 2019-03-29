3eb79a7ff6d6971e0340648a3d6792ea3b6691f2
Joe Spadafora <spadafiva@users.noreply.github.com>
2019-03-28 21:09:03 -0400

    Fix existing attributed links being removed when setting attributed text (#18)
    * check for existing links in checkText()
    
    * Remove unnecessary closure parens
    
    * Uses Set for text checking and adds tests
    
    * Adds test case for html generated attributed strings
    
    * Tweaks tests as per feedback from Chris

---
+++ Nantes/NantesTests/AttributedStringExtensionTests.swift
@@ -0,0 +1,67 @@
+//
+//  AttributedStringExtensionTests.swift
+//  NantesTests
+//
+//  Created by Joseph Spadafora on 3/28/19.
+//  Copyright © 2019 Instacart. All rights reserved.
+//
+
+import XCTest
+@testable import Nantes
+
+final class AttributedStringExtensionTests: XCTestCase {
+    
+    private let testUrl = URL(string: "https://www.swiftjoe.com")!
+    
+    func testReturnsLinksIfFoundInAttributedString() {
+        let linkString = NSAttributedString(string: "Contains a link", attributes: [.link: testUrl])
+        let existingLinks = linkString.findExistingLinks()
+        XCTAssertFalse(existingLinks.isEmpty)
+    }
+    
+    func testFindsLinksForAttributedStringsCreatedFromHTML() {
+        let htmlString = "<a href=\"http://www.google.com\">I can be clicked\""
+        guard let data = htmlString.data(using: .utf8) else {
+            XCTFail()
+            return
+        }
+        do {
+            let attributedString = try NSAttributedString(data: data,
+                                                          options: [.documentType: NSAttributedString.DocumentType.html,
+                                                                    .characterEncoding: String.Encoding.utf8.rawValue],
+                                                          documentAttributes: nil)
+            let existingLinks = attributedString.findExistingLinks()
+            XCTAssert(existingLinks.count == 1)
+        } catch {
+            XCTFail()
+        }
+    }
+    
+    func testReturnsLinkIsEmptyIfNoAttributedLink() {
+        let linkString = NSAttributedString(string: "Contains no links at all")
+        let checkingResults = linkString.findExistingLinks()
+        XCTAssertTrue(checkingResults.isEmpty)
+    }
+    
+    func testCheckingResultsIsEmptyIfAttributedStringContainsAttributedLink() {
+        do {
+            let detector = try NSDataDetector(types: NantesLabel().enabledTextCheckingTypes.rawValue)
+            let linkString = NSAttributedString(string: "Contains a link", attributes: [.link: testUrl])
+            let checkingResults = linkString.findCheckingResults(usingDetector: detector)
+            XCTAssertTrue(checkingResults.isEmpty)
+        } catch {
+            XCTFail()
+        }
+    }
+    
+    func testCheckingResultsContainsValueIfDataFoundInString() {
+        do {
+            let detector = try NSDataDetector(types: NantesLabel().enabledTextCheckingTypes.rawValue)
+            let linkString = NSAttributedString(string: "867-5309")
+            let checkingResults = linkString.findCheckingResults(usingDetector: detector)
+            XCTAssert(checkingResults.count == 1)
+        } catch {
+            XCTFail()
+        }
+    }
+}
